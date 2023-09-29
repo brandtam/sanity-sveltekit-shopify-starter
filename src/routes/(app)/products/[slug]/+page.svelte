b05da2d084b7aef2d54f2bf57494e63e3a8a78ba
Brandt Milczewski <brandt.milczewski@shopify.com>
2023-09-29 09:56:08 -0700

    fix: remove debug

--- src/routes/(app)/products/[slug]/+page.svelte
+++ src/routes/(app)/products/[slug]/+page.svelte
@@ -206,8 +206,7 @@
 								})}
 							type="submit"
 							class="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
-							>Add to bag - selectedVariant= {selectedVariant ||
-								product.store.variants[0].store.gid}</button
+							>Add to bag</button
 						>
 					</div>
 				</form>
