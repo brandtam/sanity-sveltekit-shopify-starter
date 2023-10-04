dffe0a59475de449864c3ed401cef841ddf92d3e
Brandt Milczewski <brandt.milczewski@shopify.com>
2023-10-04 08:36:23 -0700

    feat: refactor shopify storefront api functions for consistency and readability.

--- src/lib/utils/shopify.ts
+++ src/lib/utils/shopify.ts
@@ -4,6 +4,8 @@ const accessToken = import.meta.env.VITE_SHOPIFY_STOREFRONT_API_TOKEN;
 const shopifyEndpoint = `https://${storeUrl}/api/${apiVersion}/graphql.json`;
 const shopifyInactive = !accessToken;
 
+const gql = String.raw;
+
 import { shopCart } from '$lib/stores';
 import { goto } from '$app/navigation';
 
@@ -23,7 +25,7 @@ async function shopifyFetch({ query, variables }: ShopifyFetch) {
 			body: await result.json()
 		};
 	} catch (error) {
-		console.error('Error:', error);
+		console.log('shopifyFetch error', error);
 		return {
 			status: 500,
 			error: 'Error receiving data'
@@ -35,43 +37,39 @@ export async function shopifyCreateCart() {
 	if (shopifyInactive) {
 		return;
 	}
-	try {
-		const response = await shopifyFetch({
-			query: `
-				mutation calculateCart($lineItems: [CartLineInput!]) {
-					cartCreate(input: { lines: $lineItems }) {
-						cart {
-							checkoutUrl
-							id
-							cost {
-								totalAmount {
-									amount
-								}
-							}
-							totalQuantity
-							lines(first: 100) {
-								edges {
-									node {
+	const query = gql`
+		mutation calculateCart($lineItems: [CartLineInput!]) {
+			cartCreate(input: { lines: $lineItems }) {
+				cart {
+					checkoutUrl
+					id
+					cost {
+						totalAmount {
+							amount
+						}
+					}
+					totalQuantity
+					lines(first: 100) {
+						edges {
+							node {
+								id
+								quantity
+								merchandise {
+									... on ProductVariant {
 										id
-										quantity
-										merchandise {
-											... on ProductVariant {
-												id
-												title
-												priceV2 {
-													amount
-												}
-												product {
-													id
-													title
-													handle
-													totalInventory
-													images(first: 1) {
-														edges {
-															node {
-																originalSrc
-															}
-														}
+										title
+										priceV2 {
+											amount
+										}
+										product {
+											id
+											title
+											handle
+											totalInventory
+											images(first: 1) {
+												edges {
+													node {
+														originalSrc
 													}
 												}
 											}
@@ -82,55 +80,57 @@ export async function shopifyCreateCart() {
 						}
 					}
 				}
-			`,
-			variables: {}
+			}
+		}
+	`;
+	const variables = {};
+	try {
+		const response = await shopifyFetch({
+			query,
+			variables
 		});
 		const cart = response.body.data.cartCreate.cart;
 		shopCart.set(cart);
 		return cart;
 	} catch (error) {
-		console.log(error);
+		console.log('shopifyCreateCart error', error);
 	}
 }
 
 export async function shopifyAddToCart({ cartId, variantId, quantity = 1 }: ShopifyCartAdd) {
-	try {
-		const response = await shopifyFetch({
-			query: `
-				mutation addToCart($cartId: ID!, $lines: [CartLineInput!]!) {
-					cartLinesAdd(cartId: $cartId, lines: $lines) {
-						cart {
-							checkoutUrl
-							id
-							cost {
-								totalAmount {
-									amount
-								}
-							}
-							totalQuantity
-							lines(first: 100) {
-								edges {
-									node {
+	const query = gql`
+		mutation addToCart($cartId: ID!, $lines: [CartLineInput!]!) {
+			cartLinesAdd(cartId: $cartId, lines: $lines) {
+				cart {
+					checkoutUrl
+					id
+					cost {
+						totalAmount {
+							amount
+						}
+					}
+					totalQuantity
+					lines(first: 100) {
+						edges {
+							node {
+								id
+								quantity
+								merchandise {
+									... on ProductVariant {
 										id
-										quantity
-										merchandise {
-											... on ProductVariant {
-												id
-												title
-												priceV2 {
-													amount
-												}
-												product {
-													id
-													title
-													handle
-													totalInventory
-													images(first: 1) {
-														edges {
-															node {
-																originalSrc
-															}
-														}
+										title
+										priceV2 {
+											amount
+										}
+										product {
+											id
+											title
+											handle
+											totalInventory
+											images(first: 1) {
+												edges {
+													node {
+														originalSrc
 													}
 												}
 											}
@@ -141,20 +141,24 @@ export async function shopifyAddToCart({ cartId, variantId, quantity = 1 }: Shop
 						}
 					}
 				}
-			`,
-			variables: {
-				cartId: cartId,
-				lines: [
-					{
-						merchandiseId: variantId,
-						quantity: quantity
-					}
-				]
 			}
+		}
+	`;
+	const variables = {
+		cartId: cartId,
+		lines: [
+			{
+				merchandiseId: variantId,
+				quantity: quantity
+			}
+		]
+	};
+	try {
+		const response = await shopifyFetch({
+			query,
+			variables
 		});
-		const cart = response.body.data.cartLinesAdd.cart;
-		shopCart.set(cart);
-
+		shopCart.set(response.body.data.cartLinesAdd.cart);
 		setTimeout(() => goto('/cart'), 0);
 	} catch (error) {
 		console.log('shopifyAddToCart error', error);
@@ -167,42 +171,39 @@ export async function shopifyUpdateCart({
 	variantId,
 	quantity
 }: ShopifyCartLineInput) {
-	try {
-		const response = await shopifyFetch({
-			query: `
-			mutation cartLinesUpdate($cartId: ID!, $lines: [CartLineUpdateInput!]!) {
-				cartLinesUpdate(cartId: $cartId, lines: $lines) {
-					cart {
-						checkoutUrl
-						id
-						cost {
-							totalAmount {
-								amount
-							}
+	const query = gql`
+		mutation cartLinesUpdate($cartId: ID!, $lines: [CartLineUpdateInput!]!) {
+			cartLinesUpdate(cartId: $cartId, lines: $lines) {
+				cart {
+					checkoutUrl
+					id
+					cost {
+						totalAmount {
+							amount
 						}
-						totalQuantity
-						lines(first: 100) {
-							edges {
-								node {
-									id
-									quantity
-									merchandise {
-										... on ProductVariant {
+					}
+					totalQuantity
+					lines(first: 100) {
+						edges {
+							node {
+								id
+								quantity
+								merchandise {
+									... on ProductVariant {
+										id
+										title
+										priceV2 {
+											amount
+										}
+										product {
 											id
 											title
-											priceV2 {
-												amount
-											}
-											product {
-												id
-												title
-												handle
-												totalInventory
-												images(first: 1) {
-													edges {
-														node {
-															originalSrc
-														}
+											handle
+											totalInventory
+											images(first: 1) {
+												edges {
+													node {
+														originalSrc
 													}
 												}
 											}
@@ -212,46 +213,57 @@ export async function shopifyUpdateCart({
 							}
 						}
 					}
-					userErrors {
-						field
-						message
-					}
+				}
+				userErrors {
+					field
+					message
 				}
 			}
-		`,
-			variables: {
-				cartId: cartId,
-				lines: [
-					{
-						id: lineId,
-						merchandiseId: variantId,
-						quantity: quantity
-					}
-				]
+		}
+	`;
+	const variables = {
+		cartId: cartId,
+		lines: [
+			{
+				id: lineId,
+				merchandiseId: variantId,
+				quantity: quantity
 			}
+		]
+	};
+	try {
+		const response = await shopifyFetch({
+			query,
+			variables
 		});
-		const cart = response.body.data.cartLinesUpdate.cart;
-		shopCart.set(cart);
+		shopCart.set(response.body.data.cartLinesUpdate.cart);
 	} catch (error) {
-		console.error(error);
+		console.log('shopifyUpdateCart error', error);
 	}
 }
 
 export async function shopifyRemoveCartLines({ cartId, lineIds }: ShopifyCartLines) {
-	return shopifyFetch({
-		query: `
-			mutation cartLinesRemove($cartId: ID!, $lines: [ID!]!) {
-				cartLinesRemove(cartId: $cartId, lineIds: $lineIds) {
-					userErrors {
-						field
-						message
-					}
+	const query = gql`
+		mutation cartLinesRemove($cartId: ID!, $lines: [ID!]!) {
+			cartLinesRemove(cartId: $cartId, lineIds: $lineIds) {
+				userErrors {
+					field
+					message
 				}
 			}
-		`,
-		variables: {
-			cartId: cartId,
-			lineIds: [lineIds]
 		}
-	});
+	`;
+	const variables = {
+		cartId: cartId,
+		lineIds: [lineIds]
+	};
+	try {
+		const response = await shopifyFetch({
+			query,
+			variables
+		});
+		shopCart.set(response.body.data.cartLinesUpdate.cart);
+	} catch (error) {
+		console.log('shopifyRemoveCartLines error', error);
+	}
 }
